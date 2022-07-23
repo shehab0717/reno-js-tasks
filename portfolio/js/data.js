79c431abeeb5a93d2c4757ee48ed8738e223810a
Shehab Abdullah <Shehab.Abdullah@itworx.com>
2022-07-23 23:38:23 +0200

    finished carousel and nav-bar

--- portfolio/js/data.js
+++ portfolio/js/data.js
@@ -1,38 +1,76 @@
 var data = {
-    'categories':[
+    'categories': [
         {
-            'name':'All Categories',
-            'images':[
+            'name': 'All Categories',
+            'images': [
                 'https://thedispatch.blob.core.windows.net/thedispatchimages/2022/04/1913623.jpeg',
                 'https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/325466_1100-800x825.jpg',
                 'https://www.happybrainscience.com/wp-content/uploads/2017/07/derwent-morning-Cropped.jpg',
             ]
         },
         {
-            'name':'Web Design',
-            'images':[
+            'name': 'Web Design',
+            'images': [
                 'https://thedispatch.blob.core.windows.net/thedispatchimages/2022/04/1913623.jpeg',
                 'https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/325466_1100-800x825.jpg',
                 'https://www.happybrainscience.com/wp-content/uploads/2017/07/derwent-morning-Cropped.jpg',
             ]
         },
         {
-            'name':'Branding',
-            'images':[
+            'name': 'Branding',
+            'images': [
                 'https://www.we-love-nature.de/wp-content/uploads/2018/03/Wie-der-Mensch-in-der-Natur-Kraft-sch%C3%B6pft.jpg',
                 'https://www.iucn.org/sites/default/files/styles/what_we_do_large/public/images-themes/biodiversity-shutterstock_1477256246.jpg.webp?itok=4i9JdtFu',
                 'https://www.pbs.org/wnet/nature/files/2019/07/Super-Hummingbirds-2-1280x675.jpg'
             ]
         },
         {
-            'name':'Mockups',
-            'images':[]
+            'name': 'Mockups',
+            'images': []
         },
         {
-            'name':'Photography',
-            'images':[]
+            'name': 'Photography',
+            'images': []
         },
     ],
+    'reviews': [
+        {
+            'img':'https://preview.keenthemes.com/metronic-v4/theme_rtl/assets/pages/media/profile/profile_user.jpg',
+            'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio reiciendis voluptate vitae?',
+            'name': 'Ahmed Ali',
+            'title': 'CEO, Author, inc'
+        },
+        {
+            'img':'https://preview.keenthemes.com/metronic-v4/theme_rtl/assets/pages/media/profile/profile_user.jpg',
+            'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio reiciendis voluptate vitae?',
+            'name': 'Ahmed Ali',
+            'title': 'CEO, Author, inc'
+        },
+        {
+            'img':'https://preview.keenthemes.com/metronic-v4/theme_rtl/assets/pages/media/profile/profile_user.jpg',
+            'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio reiciendis voluptate vitae?',
+            'name': 'Ahmed Ali',
+            'title': 'CEO, Author, inc'
+        },
+        {
+            'img':'../assets/me.jpg',
+            'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio reiciendis voluptate vitae?',
+            'name': 'Ahmed Ali',
+            'title': 'CEO, Author, inc'
+        },
+        {
+            'img':'../assets/me.jpg',
+            'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio reiciendis voluptate vitae?',
+            'name': 'Ahmed Ali',
+            'title': 'CEO, Author, inc'
+        },
+        {
+            'img':'../assets/me.jpg',
+            'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio reiciendis voluptate vitae?',
+            'name': 'Ahmed Ali',
+            'title': 'CEO, Author, inc'
+        },
+    ]
 }
 
-export{data}
+export { data }
--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,5 +1,13 @@
 import { data } from "./data.js";
 
+
+var navLinks = document.querySelectorAll('.header a');
+navLinks.forEach(function (navLink) {
+    navLink.onclick = (e) => {
+        document.querySelector('.header .selected-link').classList.remove('selected-link');
+        e.target.classList.add('selected-link');
+    }
+})
 function loadData() {
     var recent = document.getElementById('recent');
     var tabsContainer = document.createElement('div');
@@ -7,7 +15,7 @@ function loadData() {
     for (let i = 0; i < data.categories.length; i++) {
         var link = document.createElement('a');
         link.classList.add('tab-link');
-        link.setAttribute('data-index',i);
+        link.setAttribute('data-index', i);
         link.innerHTML = data.categories[i].name;
         if (i == 0) {
             link.classList.add('selected-link');
@@ -20,10 +28,62 @@ function loadData() {
     var tabView = document.createElement('div');
     tabView.className = 'row tab-view';
     recent.appendChild(tabView);
-    selectTab(0);
+    viewTab(0);
 
+    var carousel = document.getElementById('carousel');
+    var row = document.createElement('div');
+    var carouselRows = 0;
+    row.id = `row-${carouselRows}`;
+    row.className = 'row';
+    for (let i = 0; i < data.reviews.length; i++) {
+        row.innerHTML += reviewCard(data.reviews[i]);
+        if (i % 3 == 2) {
+            carousel.appendChild(row);
+            row = document.createElement('row');
+            row.className = 'row hide';
+            carouselRows++;
+            row.id=`row-${carouselRows}`;
+        }
+    }
 }
-function selectTab(index){
+var currentRow = 0;
+var carouselRows;
+function next(e){
+    console.log('Hi');
+    e.preventDefault();
+    slide(1);
+}
+function previous(e){
+    e.preventDefault();
+    slide(-1);
+}
+document.querySelector('#carousel .right').onclick = next;
+document.querySelector('#carousel .left').onclick = previous;
+
+function slide(direction){
+    var targetRow = currentRow+direction;
+    if(targetRow > carouselRows || targetRow < 0)
+        return;
+    
+    document.getElementById(`row-${targetRow}`).classList.remove('hide');
+    document.getElementById(`row-${currentRow}`).classList.add('hide');
+    currentRow = targetRow;
+}
+function reviewCard(reviewData) {
+    return `<div class="col-12 col-md-4 mb-4 mb-md-0">
+    <div class="card shadow-sm text-center p-4">
+        <div class="rounded-circle m-auto overflow-hidden img-wrapper mb-3">
+            <img class="w-100" src="${reviewData.img}" alt="">
+        </div>
+        <p>
+            ${reviewData.content}
+        </p>
+        <div class="fw-bold">${reviewData.name}</div>
+        <div>${reviewData.title}</div>
+    </div>
+</div>`;
+}
+function viewTab(index) {
     var tabView = document.querySelector('#recent .tab-view');
     tabView.innerHTML = '';
     data.categories[index].images.forEach((imgSrc) => {
@@ -31,16 +91,16 @@ function selectTab(index){
             `<div class="col-12 col-md-4 mb-4 mb-md-0">
                 <img class="rounded-3 w-100" src="${imgSrc}" alt="">
             </div>`;
-            // recent.innerHTML+=card; Weired behavior
-            var div = document.createElement('div');
-            div.innerHTML = card;
-            tabView.appendChild(div.firstChild);
+        // recent.innerHTML+=card; Weired behavior
+        var div = document.createElement('div');
+        div.innerHTML = card;
+        tabView.appendChild(div.firstChild);
     });
 }
 function changeTab(e) {
     e.preventDefault();
     var index = e.target.getAttribute('data-index');
-    selectTab(index);
+    viewTab(index);
     var currentTab = document.querySelector('#recent .selected-link');
     currentTab.classList.remove('selected-link');
     e.target.classList.add('selected-link');
