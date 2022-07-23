906d50c33f7f06e56865508c99859cae9f156938
Shehab Abdullah <Shehab.Abdullah@itworx.com>
2022-07-23 13:04:51 +0200

    fix not working style of selected link

---
+++ portfolio/js/data.js
@@ -0,0 +1,36 @@
+var data = {
+    'categories':[
+        {
+            'name':'All Categories',
+            'images':[
+                'https://thedispatch.blob.core.windows.net/thedispatchimages/2022/04/1913623.jpeg',
+                'https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/325466_1100-800x825.jpg',
+                'https://www.happybrainscience.com/wp-content/uploads/2017/07/derwent-morning-Cropped.jpg',
+            ]
+        },
+        {
+            'name':'Web Design',
+            'images':[
+                'https://thedispatch.blob.core.windows.net/thedispatchimages/2022/04/1913623.jpeg',
+                'https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/325466_1100-800x825.jpg',
+                'https://www.happybrainscience.com/wp-content/uploads/2017/07/derwent-morning-Cropped.jpg',
+            ]
+        },
+        {
+            'name':'Branding',
+            'images':[
+                'https://www.we-love-nature.de/wp-content/uploads/2018/03/Wie-der-Mensch-in-der-Natur-Kraft-sch%C3%B6pft.jpg',
+                'https://www.iucn.org/sites/default/files/styles/what_we_do_large/public/images-themes/biodiversity-shutterstock_1477256246.jpg.webp?itok=4i9JdtFu',
+                'https://www.pbs.org/wnet/nature/files/2019/07/Super-Hummingbirds-2-1280x675.jpg'
+            ]
+        },
+        {
+            'name':'Mockups',
+            'images':[]
+        },
+        {
+            'name':'Photography',
+            'images':[]
+        },
+    ],
+}
